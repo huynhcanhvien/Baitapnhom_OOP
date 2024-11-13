be3655b2d2af1d1ca05ba713b5c9a87eb76d8bec
huynhcanhvien <huynhcanhvien@gmail.com>
2024-11-13 17:06:04 +0700

    Update Demo.cpp

--- Demo.cpp
+++ Demo.cpp
@@ -1,25 +1,59 @@
 #include <iostream>
 #include "LLKHCaNhan.hpp"
-
 using namespace std;
 
-int main(){
-    LLKHCaNhan l("Phạm Quang Thắng");
-    BaiBao b;
-    Sach s;
-    BaiBao* bp = &b;
-    Sach* sp = &s;
-    b.change("B001",Date(1,3,1995),true,"Nguyên lý hệ điều hành",1);
-    l.append(bp);
-    s.change("S001",Date(17,8,2001),false,"Sách chuyên khảo");
-    l.append(sp);
-    s.change("S002",Date(22,11,2006),false,"Sách tham khảo");
-    l.append(sp);
-    cout << l;
-    l.update("B001");
-    cout << l[2];
-
-    l.remove(l.search("S001")->getID());
-    cout << l;
-    return 0;
+int main(int argc, const char * argv[]) {
+    LLKHCaNhan Lylich("Nhom");
+    
+    while(true){
+        cout << "Ban muon them bai bao hay sach vao LLKH?\n";
+        cout << "1.Bai Bao: nhan 1\n2.Sach: nhan 2\n3. Nhan nut bat ky de ket thuc!\n";
+        int choose;
+        cin >> choose;
+        if(choose == 1){
+            BaiBao a;
+            cin >> a;
+            Lylich.append(&a);
+        }
+        else if(choose == 2){
+            Sach a;
+            cin >> a;
+            Lylich.append(&a);
+        }
+        else{
+            break;
+        }
+    }
+    cout << "Update một CTKH trong lý lịch:\n";
+    cout << "Nhập ID: ";
+    string id;
+    cin >> id;
+    try{
+        Lylich.update(id);
+        cout << "CTKH vừa được update:\n";
+    }
+    catch(runtime_error& e){
+        cout << e.what();
+    }
+    cout << *(Lylich.search(id));
+    cout << "Xoa mot CTKH trong lý lịch:\n";
+    cout << "Nhập ID: ";
+    cin >> id;
+    try{
+        Lylich.remove(id);
+        cout << "Đã xoá thành công\n!";
+    }
+    catch(runtime_error& e){
+        cout << e.what();
+    }
+    cout << "In ra toàn bộ CTKH của lý lịch:\n";
+    cout << Lylich;
+    cout << "Nhap ID của CTKH bạn muốn xem!: ";
+    cin >> id;
+    cout << *(Lylich[id]);
+    cout << "Nhap số thứ tự của CTKH bạn muốn xem: ";
+    int index;
+    cin >> index;
+    cout << *(Lylich[index]);
+    cout << "DEMO ENDED";
 }
