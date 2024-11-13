91e07dda55af51a72632ed12b0526c17422b3086
huynhcanhvien <huynhcanhvien@gmail.com>
2024-11-13 16:22:47 +0700

    Add files via upload

---
+++ LLKHCaNhan.hpp
@@ -0,0 +1,39 @@
+#ifndef LLKHCANHAN_HPP__
+
+#define LLKHCANHAN_HPP__
+
+#include "LinkList_T.hpp"
+#include "CTKH.hpp"
+
+class LLKHCaNhan 
+{
+    public:
+        LLKHCaNhan();  // Constructor mac dinh
+        LLKHCaNhan(const LLKHCaNhan& llkh); // Constructor sao chep
+        LLKHCaNhan(const string& name); // Constructor chi nhan vao 1 tham so name
+        LLKHCaNhan(string&& name); // Constructor nhan vao 1 tham so name
+        LLKHCaNhan(const string& name, LinkList<CTKH*>* constructList); // nhan vao tham so name va list
+        LLKHCaNhan(string&& name, LinkList<CTKH*> constructList); // nhan vao tham so name va list
+
+        bool append(CTKH* newConstruct);
+        bool remove(string ID);
+        bool update(string ID);
+        CTKH*& search(string ID);
+
+        CTKH*& operator [ ] (string ID);
+        CTKH*& operator [ ] (size_t index);
+        
+        LLKHCaNhan& operator = (const LLKHCaNhan& llkh);
+
+        friend istream& operator >> (istream& in, LLKHCaNhan& llcn);
+        friend ostream& operator << (ostream& in, const LLKHCaNhan& llcn);
+
+    private:
+        string m_name_;
+        int m_nScientificContructsNumber_;
+
+        LinkList<CTKH*>* m_pConstructList_; // Danh sach con tro CTKH
+};
+
+
+#endif
